fc9e7ffae15333b02a0f86d1045af7f52bcc0ab8
priya <priya1803singh@gmail.com>
2024-01-23 17:06:53 +0530

    files updated

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 import google.generativeai as genai
+from src.logger import logging
+import json
+import PyPDF2 as pdf
+from src.utils import get_gemini_response, input_pdf_text
 
 load_dotenv()
 
@@ -11,15 +15,32 @@ genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
 ## Prompt Template
 
-input_prompt =f"""
+input_prompt ="""
 You are a skilled in ATS(Application Tracking System).
-You are a expert in technical field, software engineering, \
-data science and data analyst.
-
+You are a expert in technical field, software engineering, 
+data science and data analyst. Your task is to evaluate the resume
+based on the given JD(Job Description). You should consider that the job market is
+very competitive and you should  provide the best assistance for improving the resumes.
+Assign the percentage matching based on the JD and the missing key words with high accuracy.
+resume: {text}
+description: {JD}
+I want the response in one single string having the structure {{"JD Match": "%", "MissingKeywords: []", "Profile Summary": ""}}
 
 
 
 """
 
 
+st.title('ATS- GeminiPro')
+st.text('Improve your Resume')
 
+JD = st.text_area('Paste the Job Description')
+uploaded_file = st.file_uploader("Upload your Resume", type="pdf", help="Please uploade the PDF")
+
+submit = st.button('Submit')
+
+if submit:
+    if uploaded_file is not None:
+        text = input_pdf_text(uploaded_file)
+        response = get_gemini_response(input_prompt)
+        st.subheader(response)
--- src/utils.py
+++ src/utils.py
@@ -3,23 +3,22 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 import json
 import PyPDF2 as pdf
+from src.logger import logging
 
 
-load_dotenv()
-
-## Configure API Key
-genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
 def get_gemini_response(input):
-    model = genai.GenerativeModel('gemini_pro')
+    model = genai.GenerativeModel('gemini-pro')
     response = model.generate_content(input)
-    return json.loads(response)
+    return response.text
 
 
 def input_pdf_text(uploaded_file):
     reader = pdf.PdfReader(uploaded_file)
+    logging.info('reading pdf')
     text = ""
     for page in range(len(reader.pages)):
         page = reader.pages[page]
+        logging.info(page)
         text += str(page.extract_text())
     return text
